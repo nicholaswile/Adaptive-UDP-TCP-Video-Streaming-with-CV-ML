1475ce367caa17fbb63e8f3f39381f020aac3e9f
Nicholas Wile <74445404+nicholaswile@users.noreply.github.com>
2024-04-12 21:09:22 -0400

    visualize pixel diff

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Adaptive-UDP-TCP-Video-Streaming-with-CV-ML
 A prototype hybrid UDP/TCP video streaming application. We use the OpenCV computer vision library to calculate pixel difference between lecture video frames. We leverage this to identify key frames in a video stream and adapt the transport layer protocol accordingly for real-time video streaming. Keyframes will be sent with TCP and less important frames will be sent with UDP.
+
+Create a virtual environment, activate it, and run the following command:
+```
+pip install -r requirements.txt
+```
+
+Then run the command:
+```
+python keyframe_detect.py
+```
--- keyframe_detect.py
+++ keyframe_detect.py
@@ -6,61 +6,87 @@ Spring 2024
 '''
 
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
+
+fps = 30.0
+threshold = 2000000
 
 # Read the input video
 vid_dir= "test-vids/"
-vid_names = ["lecture0", ]
-in_vid_path = f"{vid_dir}{vid_names[0]}.mp4"
-cap = cv2.VideoCapture(in_vid_path)
+fig_dir = "figures/"
+vid_names = ["lecture0", "lecture1"]
 
-# Initialize variables
-frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-threshold = 2000000
-keyframe_indices = []
+for vid_name in vid_names:
+    in_vid_path = f"{vid_dir}{vid_name}.mp4"
+    cap = cv2.VideoCapture(in_vid_path)
 
-# Outputs a video that labels keyframes
-out_vid_path = vid_dir + f"{vid_names[0]}_labeled.mp4"
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-video = cv2.VideoWriter(out_vid_path, fourcc, 30.0, (width, height))
+    # Initialize variables
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    keyframe_indices = []
+    pixel_values = []
 
-max = 0
+    # Outputs a video that labels keyframes
+    out_vid_path = vid_dir + f"{vid_name}_labeled.mp4"
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    video = cv2.VideoWriter(out_vid_path, fourcc, fps, (width, height))
 
-# Iterate thru each vid frame and detect key frame
-for i in range(frame_count):
-    ret, this_frame = cap.read()
-    if not ret:
-        break
-    
-    if i == 0:
-        keyframe_indices.append(i)
-        cv2.putText(this_frame, f"Frame {i}: Keyframe (TCP)", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    # Iterate thru each vid frame and detect key frame
+    for i in range(frame_count):
+        if i == 4000:
+            frame_count = 4000
+            # Don't go beyond 4K frames, b/c I don't feel like waiting
+            break
 
-    # Pixel difference 
-    if i > 0:
-        diff = cv2.absdiff(prev_frame, this_frame)
-        diff_sum = np.sum(diff)
-        if (diff_sum > max):
-            print(diff_sum)
-            max = diff_sum
-        '''
-        Compare pixel diff to threshold
-        If diff exceeds threshold, more changes
-        So this gets labeled as a keyframe
-        '''
-        if diff_sum > threshold:
+        ret, this_frame = cap.read()
+        if not ret:
+            break
+        
+        # Label first frame as key frame
+        if i == 0:
             keyframe_indices.append(i)
+            pixel_values.append(0)
+            prev_frame = this_frame.copy()
             cv2.putText(this_frame, f"Frame {i}: Keyframe (TCP)", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        else:
-            cv2.putText(this_frame, f"Frame {i}: Not Key (UDP)", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-    prev_frame = this_frame.copy()
-    video.write(this_frame)
+        # Pixel difference 
+        if i > 0:
+            diff = cv2.absdiff(prev_frame, this_frame)
+            diff_sum = np.sum(diff)
+            pixel_values.append(diff_sum)
+            prev_frame = this_frame.copy()
 
-cap.release()
-video.release()
+            '''
+            Compare pixel diff to threshold
+            If diff exceeds threshold, more changes
+            So this gets labeled as a keyframe
+            '''
+            if diff_sum > threshold:
+                keyframe_indices.append(i)
+                cv2.putText(this_frame, f"Frame {i}: Keyframe (TCP)", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                cv2.putText(this_frame, f"Pixel Diff: {diff_sum}", (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
-print(f"Detected {len(keyframe_indices)} keyframes out of {frame_count} total video frames.")
-print(f"Output video saved as {out_vid_path}")
+            else:
+                cv2.putText(this_frame, f"Frame {i}: Not Key (UDP)", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                cv2.putText(this_frame, f"Pixel Diff: {diff_sum}", (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+        video.write(this_frame)
+
+    cap.release()
+    video.release()
+
+    # Plot distribution of pixel diff across video frames
+    fig, ax = plt.subplots()
+    ax.plot(np.linspace(0, frame_count, frame_count), pixel_values, linewidth=1.0)
+    plt.ylim(0,threshold*5)
+    plt.axhline(threshold, color = 'r', linestyle = '-') 
+    plt.title(f"Pixel Difference Distribution: {vid_name}\n({len(keyframe_indices)} keyframes)")
+    plt.xlabel("Video Frame")
+    plt.ylabel("Pixel Difference")
+    plt.savefig(f"{fig_dir}{vid_name}.png")
+    plt.show()
+
+    print(f"Detected {len(keyframe_indices)} keyframes out of {frame_count} total video frames.")
+    print(f"Output video saved as {out_vid_path}")
